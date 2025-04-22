1f5b5bfffe4f52eaa4d838ce708e99e15c461d36
DangPham112000 <dangpham112000@gmail.com>
2025-04-22 16:18:17 +0700

    add left bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,13 +11,16 @@ export default function RootLayout({
     <html lang="en">
       <body className="">
         <div className="xxl:max-w-screen-xxl xl:max-w-screen-xl lg:max-w-screen-lg max-w-screen-md mx-auto flex justify-between">
-          <div className="bg-red-200 h-screen xxl:px-8 xsm:px-4 px-2">
+          {/* bg-red-200 */}
+          <div className="xxl:px-8 xsm:px-4 px-2">
             <LeftBar />
           </div>
-          <div className="bg-blue-200 h-screen flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray">
+          {/* bg-blue-200 */}
+          <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray">
             {children}
           </div>
-          <div className="bg-green-200 h-screen flex-1 lg:flex hidden md:ml-8 ml-4">
+          {/* bg-green-200 */}
+          <div className="flex-1 lg:flex hidden md:ml-8 ml-4">
             <RightBar />
           </div>
         </div>
--- src/components/LeftBar.tsx
+++ src/components/LeftBar.tsx
@@ -1,5 +1,125 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
+const menuList = [
+  {
+    id: 1,
+    name: "Homepage",
+    link: "/",
+    icon: "home.svg",
+  },
+  {
+    id: 2,
+    name: "Explore",
+    link: "/",
+    icon: "explore.svg",
+  },
+  {
+    id: 3,
+    name: "Notification",
+    link: "/",
+    icon: "notification.svg",
+  },
+  {
+    id: 4,
+    name: "Messages",
+    link: "/",
+    icon: "message.svg",
+  },
+  {
+    id: 5,
+    name: "Bookmarks",
+    link: "/",
+    icon: "bookmark.svg",
+  },
+  {
+    id: 6,
+    name: "Jobs",
+    link: "/",
+    icon: "job.svg",
+  },
+  {
+    id: 7,
+    name: "Communities",
+    link: "/",
+    icon: "community.svg",
+  },
+  {
+    id: 8,
+    name: "Premium",
+    link: "/",
+    icon: "logo.svg",
+  },
+  {
+    id: 9,
+    name: "Profile",
+    link: "/",
+    icon: "profile.svg",
+  },
+  {
+    id: 10,
+    name: "More",
+    link: "/",
+    icon: "more.svg",
+  },
+];
+
 export const LeftBar = () => {
-  return <div>LeftBar</div>;
+  return (
+    <div className="h-screen sticky top-0 flex flex-col justify-between pt-2 pb-8">
+      {/* LOGO + MENU SECTION */}
+      <div className="flex flex-col items-center xxl:items-start gap-4 text-lg ">
+        {/* LOGO */}
+        <Link href="/" className="p-2 rounded-full hover:bg-[#181818]">
+          <Image src="/icons/logo.svg" alt="" width={24} height={24} />
+        </Link>
+        {/* MENU LIST */}
+        <div className="flex flex-col gap-4">
+          {menuList.map((menuItem) => (
+            <Link
+              href={menuItem.link}
+              id={JSON.stringify(menuItem.id)}
+              className="flex items-center gap-4 p-2 rounded-full hover:bg-[#181818]"
+            >
+              <Image
+                src={`/icons/${menuItem.icon}`}
+                alt={menuItem.name}
+                width={24}
+                height={24}
+              />
+              <span className="xxl:inline hidden">{menuItem.name}</span>
+            </Link>
+          ))}
+        </div>
+        {/* BUTTON */}
+        <Link
+          href="/"
+          className="flex justify-center items-center bg-white rounded-full xxl:w-auto w-12 xxl:h-auto h-12"
+        >
+          <Image
+            className="xxl:hidden block"
+            src="/icons/post.svg"
+            alt="new_post"
+            width={24}
+            height={24}
+          />
+          <span className="xxl:block hidden text-black font-bold py-3 px-20">Post</span>
+        </Link>
+      </div>
+      {/* USER */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <div className="w-10 h-10 relative rounded-full overflow-hidden">
+            <Image src="/tmp/avatar.png" alt="avatar" fill />
+          </div>
+          <div className="xxl:flex hidden flex-col">
+            <span className="font-bold">Dang Pham</span>
+            <span className="text-sm text-textGray">@Dante__hehe</span>
+          </div>
+        </div>
+        <div className="xxl:block hidden cursor-pointer font-bold">...</div>
+      </div>
+    </div>
+  );
 };
